c76c8e64d2bf917c590ea6c9f9778c6e5630127e
Davide <berardo.1814724@studenti.uniroma1.it>
2025-11-08 12:50:04 +0100

    Initial setup for laser callback

--- src/obstacle_avoidance/src/obstacle_avoidance.cpp
+++ src/obstacle_avoidance/src/obstacle_avoidance.cpp
@@ -1,11 +1,15 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
-        
+#include <sensor_msgs/LaserScan.h>
 
 geometry_msgs::Twist keyboard_velocity;
 
 void keyboard_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
     keyboard_velocity = *msg;
+}
+
+void laser_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
+    ROS_INFO("Laser is working");
 }
 
 int main(int argc, char** argv) {
@@ -14,7 +18,9 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
 
-    ros::Subscriber keyboard_vel_sub = nh.subscribe("keyboard_vel_call", 1, keyboard_velocity_callback);
+    ros::Subscriber keyboard_vel_sub = nh.subscribe("keyboard_vel_call", 1, keyboard_vel_callback);
+
+    ros::Subscriber laser_sub = nh.subscribe("base_scan", 1, laser_callback);
     ROS_INFO("Node started");
 
     ros::spin();
