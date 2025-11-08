89178d88f07abc40971563bd4bfc35f83490d5c6
Davide <berardo.1814724@studenti.uniroma1.it>
2025-11-08 12:34:25 +0100

    Initial keyboard velocity callback and main

--- src/obstacle_avoidance/src/obstacle_avoidance.cpp
+++ src/obstacle_avoidance/src/obstacle_avoidance.cpp
@@ -1,1 +1,23 @@
-//TODO
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+        
+
+geometry_msgs::Twist keyboard_velocity;
+
+void keyboard_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
+    keyboard_velocity = *msg;
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "obstacle_avoidance");
+    ros::NodeHandle nh;
+
+
+    ros::Subscriber keyboard_vel_sub = nh.subscribe("keyboard_vel_call", 1, keyboard_velocity_callback);
+    ROS_INFO("Node started");
+
+    ros::spin();
+
+    return 0;
+}
