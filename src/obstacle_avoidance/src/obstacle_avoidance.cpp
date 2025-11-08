63ae926650dce1ca9722674d29836898eda6ede3
Davide <berardo.1814724@studenti.uniroma1.it>
2025-11-08 13:19:30 +0100

    Adding boolean to vel and laser callback

--- src/obstacle_avoidance/src/obstacle_avoidance.cpp
+++ src/obstacle_avoidance/src/obstacle_avoidance.cpp
@@ -3,13 +3,20 @@
 #include <sensor_msgs/LaserScan.h>
 
 geometry_msgs::Twist keyboard_velocity;
+bool velocity_received = false;
 
 void keyboard_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
     keyboard_velocity = *msg;
+
+    if(keyboard_velocity.linear.x != 0 || keyboard_velocity.angular.z != 0){
+        velocity_received = true;  
+    }
 }
 
 void laser_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
+    if (!velocity_received) return;
     ROS_INFO("Laser is working");
+    velocity_received = false;
 }
 
 int main(int argc, char** argv) {
